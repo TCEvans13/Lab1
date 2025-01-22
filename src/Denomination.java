dd3c50f36dec6eb9e99ea1ff1e8ec34ea62319ec
Tyler Evans <TEVANS13@cub.uca.edu>
2025-01-21 19:10:15 -0600

    Add denomination purse and register

---
+++ src/Denomination.java
@@ -0,0 +1,21 @@
+public record Denomination() {
+    public static void main(String[] args) {
+        //arrays of strings and strings used for naming notes and coins
+         String[] billNames = {"Hundred Dollar", "Fifty Dollar", "Twenty Dollar", "Ten Dollar",
+                 "Five Dollar", "One Dollar"};
+         String[] coinNames = {"Quarter", "Dime", "Nickel", "Penny"};
+         String plural = "s";
+
+         //array containing the form of money (bill or coin)
+         String[] form = {"Bill", "Coin"};
+         //double containing the amount of money to be translated
+         Double amt = 0.0;
+
+
+
+
+
+
+
+    }
+}
